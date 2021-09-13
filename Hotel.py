77fe6082fd45a8965170f8caaa3b6a907cc69c7f
SYS_09 <suganthi96r@gmail.com>
2021-09-13 20:48:22 +0530

    basic

--- Hotel.py
+++ Hotel.py
@@ -1,9 +1,19 @@
-print("1-VEG\n2-NON_VEG")
-option==input("Enter the option")
-if option=="1":
-    print(veg)
+option=int(input("Enter the option \n1-VEG\n2-NON_VEG"))
+if option==1:
     dishes=input("enter the dishes type \n1-breakfast\n2-lunch")
     if dishes=="1":
         varities=input("enter the variable type\n1-idly------Rs.6\n2-pongal----Rs.20")
-        print("varities coming soon")
-        
+        print(f"{varities} coming soon")
+    elif dishes=="2":
+        varities=input("Enter the varities types \n1-sambar-----Rs.40 \n2-curd------Rs.30")
+        print(f"{varities} comming soon")
+elif option==2:
+    dishes = input("enter the dishes type \n1-breakfast\n2-lunch")
+    if dishes=="1":
+       varities=input("enter the option \n1-chilly idly\n2-paroota")
+       print(f"{varities} comming soon")
+    elif dishes=="2":
+       varities=input("enter the varities \n1-briyani----Rs.200\n2-meals----Rs.100")
+       print(f"{varities} comming soon")
+else:
+       print("Invalid")
